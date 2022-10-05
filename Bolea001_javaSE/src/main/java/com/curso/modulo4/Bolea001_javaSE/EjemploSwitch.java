1e40e239fcedbf261f55c3184fa1f3ee77b1b3aa
String39 <dfernandez_1983@outlook.es>
2022-10-05 13:43:08 +0200

    Create EjemploSwitch.java

---
+++ Bolea001_javaSE/src/main/java/com/curso/modulo4/Bolea001_javaSE/EjemploSwitch.java
@@ -0,0 +1,25 @@
+package com.curso.modulo4.Bolea001_javaSE;
+
+public class EjemploSwitch {
+
+	public static void main(String[] args) {
+	
+		String a = "7";
+		
+		switch (a) {
+	
+		case "1":
+			System.out.println("uno");
+			break;
+			
+		case "2":
+			System.out.println("dos");
+			break;
+			
+		default:
+			System.out.println("error");
+			break;
+		}
+	}
+
+}
