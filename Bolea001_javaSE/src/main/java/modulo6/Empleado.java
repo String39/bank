ad328837c011da1aab02fe1a9d79a90d6baf2636
String39 <dfernandez_1983@outlook.es>
2022-10-10 14:09:59 +0200

    dia 6

--- Bolea001_javaSE/src/main/java/modulo6/Empleado.java
+++ Bolea001_javaSE/src/main/java/modulo6/Empleado.java
@@ -2,4 +2,24 @@ package modulo6;
 
 public class Empleado {
 	
+	private String nombre;
+	private double salario;
+	
+	public String getDetails() {
+		return nombre + " tiene salario" + salario;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+		
 }
+
--- Bolea001_javaSE/src/main/java/modulo6/Gerente.java
+++ Bolea001_javaSE/src/main/java/modulo6/Gerente.java
@@ -1,5 +1,14 @@
 package modulo6;
 
-public class Gerente {
-
+public class Gerente extends Empleado{
+	
+	//atributos.. todos los del padre y los suyos
+	private String departamento;
+	
+	public String getDepartamento() {
+		return departamento;
+	}
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
 }
