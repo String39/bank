9310d06547e0ac30206d3dc808368904d01ea98c
String39 <dfernandez_1983@outlook.es>
2022-11-03 12:53:14 +0100

    dia 21

---
+++ Proy012_SpringBootMVC/src/main/java/com/example/spring/controller/AltaController.java
@@ -0,0 +1,17 @@
+package com.example.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.spring.entidades.Pedido;
+import com.example.spring.servicios.PedidosService;
+
+@Controller
+public class AltaController {
+
+	private PedidosService ps;
+	
+	//@GetMapping("/altaPedido")
+	
+}
