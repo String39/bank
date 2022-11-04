9e889099a9252b0729defa7459b9d74c467b1a21
String39 <dfernandez_1983@outlook.es>
2022-11-04 14:39:20 +0100

    dia 22

--- Proy012_SpringBootMVC/src/main/java/com/example/spring/restcontroller/PedidosRestFullController.java
+++ Proy012_SpringBootMVC/src/main/java/com/example/spring/restcontroller/PedidosRestFullController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,8 @@ public class PedidosRestFullController {
 	
 	
 	//DELETE
+	@DeleteMapping("/ws/pedidos/{id}")
+	
 	
 	//UPDATE
 	@PutMapping("ws/pedidos")
